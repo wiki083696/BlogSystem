5ed01c80c222dc31a5de424bfcfd53f3f41f3124
wiki <hello@wiki.cn>
2025-04-27 14:59:31 +0800

    基于Redis的高并发点评系统：实现登录功能

--- hm-dianping/src/main/java/com/hmdp/service/IUserService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
@@ -15,5 +16,12 @@ import javax.servlet.http.HttpSession;
  * @since 2021-12-22
  */
 public interface IUserService extends IService<User> {
+    /*
+    * 发送手机验证码
+    * */
     Result sendCode(String phone, HttpSession session);
+    /*
+    * 实现登录功能
+    * */
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,8 +2,10 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
+import com.hmdp.entity.UserInfo;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.util.Random;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -24,7 +28,9 @@ import java.util.Random;
 @Service
 @Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
-
+    /*
+     * 发送手机验证码
+     * */
     @Override
     public Result sendCode(String phone, HttpSession session) {
         //1.校验手机号
@@ -41,5 +47,45 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送短信验证码成功，验证码：{}",code);
         //返回ok
         return Result.ok();
+    }
+    /*
+     * 实现登录功能
+     * */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误!");
+        }
+        //2.校验验证码
+        Object cacheCode = session.getAttribute("code");//session里的验证码
+        String code = loginForm.getCode();                 //用户提交的验证码
+        if(cacheCode == null || !cacheCode.toString().equals(code)){
+            //3.不一致，报错
+            return Result.fail("验证码错误");
+        }
+        //4.一致，根据手机号查询用户 select * from tb_user where phone = ?
+        User user = query().eq("phone", phone).one();
+
+        //5.判断用户是否存在
+        if (user == null) {
+            //6.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+
+        }
+        //7.保存用户信息到session中
+        session.setAttribute("user",user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        //1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        //2.保存用户
+        save(user);
+        return user;
     }
 }
